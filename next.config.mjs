a5c47a5e314795869900b2d848c4fa29a43b08d5
dm6hkq2q7c-spec <dm6hkq2q7c@privaterelay.appleid.com>
2026-05-30 16:27:20 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',       // 关键配置：开启静态导出
+  trailingSlash: true     // 可选：让生成的文件路径带斜杠，兼容性更好
 };
 
 export default nextConfig;
