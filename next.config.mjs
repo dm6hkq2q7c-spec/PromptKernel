8ed5e3f0bc8c0ecb76976321bfb96a6754273cde
dm6hkq2q7c-spec <dm6hkq2q7c@privaterelay.appleid.com>
2026-05-30 16:59:38 +0800

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',       // 关键配置：开启静态导出
-  trailingSlash: true     // 可选：让生成的文件路径带斜杠，兼容性更好
-};
-
-export default nextConfig;
